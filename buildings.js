d7fbf87509ab94debcd38a4ea6046f3d374a3625
bloodrizer <cheshire.panther@gmail.com>
2015-07-26 20:26:06 -0700

    Adding missing energy consumption

--- buildings.js
+++ buildings.js
@@ -1178,6 +1178,7 @@ dojo.declare("classes.managers.BuildingsManager", com.nuclearunicorn.core.TabMan
 					{ name : "titanium", val: 50 }
 				],
 				effects: {
+                    "energyConsumption": 1,
 					"culturePerTickBase" : 1,
                     "happiness": 5,
 					"cultureMax" : 500
