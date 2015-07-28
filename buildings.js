2feb06cbb8d048b5b86c9abfc62f2b6ef833c4ba
bloodrizer <cheshire.panther@gmail.com>
2015-07-27 19:53:15 -0700

    Removed energy requirements from broadcast towers

--- buildings.js
+++ buildings.js
@@ -1179,7 +1179,6 @@ dojo.declare("classes.managers.BuildingsManager", com.nuclearunicorn.core.TabMan
 					{ name : "titanium", val: 50 }
 				],
 				effects: {
-                    "energyConsumption": 1,
 					"culturePerTickBase" : 1,
                     "happiness": 5,
 					"cultureMax" : 500
