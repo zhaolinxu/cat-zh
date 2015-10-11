339950c3c65c96d83d5373fd4395f6da64192b55
bloodrizer <cheshire.panther@gmail.com>
2015-10-10 22:09:51 -0700

    Adding missing files from the previous commits

---
+++ js/stats.js
@@ -0,0 +1,79 @@
+dojo.declare("classes.managers.StatsManager", com.nuclearunicorn.core.TabManager, {
+    
+    stats:
+        [{
+            name: "totalKittens",
+            title: "Total kittens",
+            val: 0
+        },{
+            name: "kittensDead",
+            title: "Kittens dead",
+            val: 0
+        },{
+            name: "totalYears",
+            title: "Total years played",
+            val: 0
+        }, {
+            name: "totalParagon",
+            title: "Total paragon",
+            val: 0
+        }, {
+            name: "eventsObserved",
+            title: "Rare events observed",
+            val: 0
+        }, {
+            name: "unicornsSacrificed",
+            title: "Unicorns sacrificed",
+            val: 0
+        }
+    ],
+    
+    load: function(saveData){
+        this.loadMetadata(this.stats, saveData.stats, ["val"]);
+    },
+    
+    save: function(saveData){
+        saveData.stats = this.filterMetadata(this.stats, ["name", "val"]);
+
+    },
+    
+    getStat: function(name){
+        return this.getMeta(name, this.stats);
+    }
+});
+
+dojo.declare("classes.tab.StatsTab", com.nuclearunicorn.game.ui.tab, {
+
+    container: null,
+
+    constructor: function(tabName){
+
+    },
+
+    render: function(content){
+        this.container = content;
+    },
+
+    update: function(){
+        
+        /*console.log(this.game, this.game.stats);*/
+
+        var stats = this.game.stats.stats;
+        
+        dojo.empty(this.container);
+        var table = dojo.create("table", null, this.container);
+        
+        for (var i in stats){
+            var tr = dojo.create("tr", null, table);
+            dojo.create("td", {
+                innerHTML: stats[i].title
+            }, tr);
+            dojo.create("td", {
+                style: {
+                  paddingLeft: "20px"  
+                },
+                innerHTML: stats[i].val
+            }, tr);
+        }    
+    }
+});
