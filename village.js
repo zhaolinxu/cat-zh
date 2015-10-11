7cef5019f06ff3b25f959b98c32a750e1a4fb156
bloodrizer <cheshire.panther@gmail.com>
2015-10-10 22:08:02 -0700

    minor stuff

--- village.js
+++ village.js
@@ -688,6 +688,9 @@ dojo.declare("com.nuclearunicorn.game.village.KittenSim", null, {
 	},
 
 	killKittens: function(amount){
+
+        this.stats.getStat("kittensDead").val += amount;
+        
 		if (amount > this.kittens.length) {
 			amount = this.kittens.length;
 		}
