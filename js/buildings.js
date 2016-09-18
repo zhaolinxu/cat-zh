b257df31d4836b6f5cbeb562531c8680d427c9cd
freeroot <free.r@gmx.fr>
2016-09-18 02:03:30 +0200

    ...

--- js/buildings.js
+++ js/buildings.js
@@ -81,7 +81,7 @@ dojo.declare("classes.managers.BuildingsManager", com.nuclearunicorn.core.TabMan
 					effect =  bld.effects[effectName];
 				// Max effects and Ratio effects depends on constructed buildings
 				} else if (effectName.indexOf("Max", effectName.length - 3) != -1 ||
-					effectName.indexOf("Ratio", effectName.length - 5) != -1)){
+					effectName.indexOf("Ratio", effectName.length - 5) != -1){
 					effect = bld.effects[effectName] * bld.val;
 				} else {
 					effect = bld.effects[effectName] * bld.on;
