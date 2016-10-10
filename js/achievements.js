d8e3eb4229d15f4ef2ee1fe152698048cc95a5b9
freeroot <free.r@gmx.fr>
2016-10-10 17:55:12 +0200

    put console.warn in loadMetadata back on and achievements cleaning

--- js/achievements.js
+++ js/achievements.js
@@ -8,88 +8,77 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
             description: "Lift the shroud of the Unicorn conspiracy!",
             condition: function () {
                 return ( this.game.resPool.get("unicorns").value > 0 );
-            },
-            unlocked: false
+            }
         }, {
             name: "uniception",
             title: "Uniception",
             description: "Find the conspiracy within the conspiracy",
             condition: function () {
                 return ( this.game.resPool.get("tears").value > 0 );
-            },
-            unlocked: false
+            }
         }, {
             name: "sinsOfEmpire",
             title: "Sins of a Solar Empire",
             description: "Wait, seriously?",
             condition: function () {
                 return ( this.game.resPool.get("alicorn").value > 0 );
-            },
-            unlocked: false
+            }
         }, {
             name: "anachronox",
             title: "Anachronox",
             description: "Please stop",
             condition: function () {
                 return ( this.game.resPool.get("timeCrystal").value > 0 );
-            },
-            unlocked: false
+            }
         }, {
             name: "deadSpace",
             title: "Dead Space",
             description: "In space no one can hear you meow.",
             condition: function () {
                 return ( this.game.resPool.get("necrocorn").value > 0 );
-            },
-            unlocked: false
+            }
         }, {
             name: "ironWill",
             title: "Iron Will",
             description: "You truly deserved this",
             condition: function () {
                 return ( this.game.ironWill && !this.game.resPool.get("kittens").value && this.game.bld.get("mine").on > 0 );
-            },
-            unlocked: false
+            }
         }, {
             name: "uberkatzhen",
             title: "Uberkatzchen",
             description: "What does not kill you makes you stronger",
             condition: function () {
                 return ( this.game.ironWill && !this.game.resPool.get("kittens").value && this.game.bld.get("warehouse").on > 0 );
-            },
-            unlocked: false
+            }
         }, {
             name: "hundredYearsSolitude",
             title: "One Hundred Years of Solitude",
             description: "How far is too far?",
             condition: function () {
                 return ( this.game.ironWill && !this.game.resPool.get("kittens").value && this.game.bld.get("steamworks").on > 0 );
-            },
-            unlocked: false
+            }
         }, {
             name: "soilUptuned",
             title: "Virgin Soil Upturned",
             description: "Have 45 pastures in Iron Will mode",
             condition: function () {
                 return ( this.game.ironWill && !this.game.resPool.get("kittens").value && this.game.bld.get("pasture").on >= 45 );
-            },
-            unlocked: false
+            }
         }, {
             name: "atlasUnmeowed",
             title: "Atlas Unmeowed",
             description: "Construct a magneto in Iron Will mode",
             condition: function () {
                 return ( this.game.ironWill && !this.game.resPool.get("kittens").value && this.game.bld.get("magneto").on > 0 );
-            },
-            unlocked: false
+            }
         }, {
             name: "meowMeowRevolution",
             title: "Meow Meow Revolution",
             description: "Construct a factory in Iron Will mode",
             condition: function () {
                 return ( this.game.ironWill && !this.game.resPool.get("kittens").value && this.game.bld.get("factory").on > 0 );
-            },
-            unlocked: false
+            }
         }, {
             name: "spaceOddity",
             title: "Space Oddity",
@@ -101,41 +90,35 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
             starCondition: function () {
                 return ( this.game.ironWill && this.game.space.getProgram("moonMission").on && this.game.resPool.get("paragon").value < 10);
             },
-            unlocked: false,
-            hasStar: true,
-            starUnlocked: false
+            hasStar: true
         }, {
             name: "jupiterAscending",
             title: "Jupiter Ascending",
             description: "Get to the space on a first year",
             condition: function () {
                 return ( this.game.space.getProgram("orbitalLaunch").on && this.game.calendar.year <= 1);
-            },
-            unlocked: false
+            }
         }, {
             name: "shadowOfTheColossus",
             title: "Shadow Of The Colossus",
             description: "Build a Ziggurat having only one kitten",
             condition: function () {
                 return ( this.game.bld.get("ziggurat").on > 0 && this.game.village.maxKittens == 1);
-            },
-            unlocked: false
+            }
         }, {
             name: "sunGod",
             title: "Sun God",
             description: "Gain a total of 696,342 accumulated faith",
             condition: function () {
                 return ( this.game.religion.faith >= 696342 );
-            },
-            unlocked: false
+            }
         }, {
             name: "heartOfDarkness",
             title: "Heart Of Darkness",
             description: "Become the chieftain of a zebra tribe. (How is this even possible?)",
             condition: function () {
                 return (this.game.resPool.get("zebras").value > 1);
-            },
-            unlocked: false
+            }
         }, {
             name: "winterIsComing",
             title: "Winter Is Coming",
@@ -143,8 +126,7 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
             unethical: true,
             condition: function () {
                 return (this.game.deadKittens >= 10);
-            },
-            unlocked: false
+            }
         }, {
             name: "youMonster",
             title: "You Monster",
@@ -152,8 +134,7 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
             description: "Poor kittens.",
             condition: function () {
                 return (this.game.deadKittens >= 100);
-            },
-            unlocked: false
+            }
         }, {
             name: "superUnethicalClimax",
             title: "Super Unethical Climax",
@@ -161,24 +142,21 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
             description: "Cheat your way through the game.",
             condition: function () {
                 return (this.game.cheatMode);
-            },
-            unlocked: false
+            }
         }, {
             name: "lotusMachine",
             title: "Lotus Eater Machine",
             description: "Break the cycle of reincarnations",
             condition: function () {
                 return (this.game.resPool.get("karma").value >= 1);
-            },
-            unlocked: false
+            }
         }, {
             name: "serenity",
             title: "Serenity",
             description: "Have 50 kittens without losing any of them",
             condition: function () {
                 return (this.game.village.getKittens() >= 50 && this.game.deadKittens == 0);
-            },
-            unlocked: false
+            }
         },
         {
             name: "utopiaProject",
@@ -191,9 +169,7 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
             starCondition: function () {
                 return (this.game.village.happiness >= 5 && this.game.resPool.get("kittens").value > 35);
             },
-            unlocked: false,
-            hasStar: true,
-            starUnlocked: false
+            hasStar: true
         }, {
             name: "cathammer",
             title: "Cathammer 40K",
@@ -205,9 +181,7 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
             starCondition: function () {
                 return (this.game.calendar.year >= 40000 + this.game.time.flux);
             },
-            unlocked: false,
-            hasStar: true,
-            starUnlocked: false
+            hasStar: true
         }, {
             name: "limitlessClicker",
             title: "Limitless Clicker",
@@ -219,9 +193,7 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
             starCondition: function () {
                 return (this.game.stats.getStat("totalClicks").val >= 1000000);
             },
-            unlocked: false,
-            hasStar: true,
-            starUnlocked: false
+            hasStar: true
         }],
 
     constructor: function (game) {
@@ -286,9 +258,7 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
 		for (var i = 0; i < this.achievements.length; i++){
 			var ach = this.achievements[i];
 			ach.unlocked = false;
-			if (ach.hasStar){
-				ach.starUnlocked = false;
-			}
+			ach.starUnlocked = false;
 		}
 	},
 
