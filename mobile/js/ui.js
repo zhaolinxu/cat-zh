c1459b92b5f84fdb1949d8f4bd089667e5ea227a
bloodrizer <cheshire.panther@gmail.com>
2017-03-12 19:58:05 -0700

    Removing obsolete folders

--- mobile/js/ui.js
+++
@@ -1,9 +0,0 @@
-dojo.declare("classes.ui.FW7UI", classes.ui.UISystem, {
-    render: function(){
-        var game = this.game;
-    },
-
-    update: function(){
-        dojo.publish("game/update", [this.game]);
-    }
-});
