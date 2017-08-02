ce0757361049559bc0ff170ea5d37a7e121ebdde
bloodrizer <cheshire.panther@gmail.com>
2017-08-01 18:16:17 -0700

    kiwiirc, yay

--- js/ui.js
+++ js/ui.js
@@ -388,8 +388,11 @@ dojo.declare("classes.ui.DesktopUI", classes.ui.UISystem, {
         var height = $(window.top).height() || 850;
         //console.log("IRC WINDOW HEIGHT:", height);
 
-        var $chat = $('#IRCChatInner');
-        swfobject.embedSWF("lib/lightirc/lightIRC.swf", $chat[0], 600, height - 150, 10, "lib/lightirc/expressInstall.swf", params);
+        var $chat = $('#IRCChatInner iframe');
+        $chat.css("height", height - 180);
+
+        //swfobject.embedSWF("lib/lightirc/lightIRC.swf", $chat[0], 600, height - 150, 10, "lib/lightirc/expressInstall.swf", params);
+        /*<iframe src="https://kiwiirc.com/client/irc.canternet.org/?nick=kitten_?#kittensgame" style="border:0; width:100%; height:450px;"></iframe>*/
         this.isChatActive = true;
     },
 
