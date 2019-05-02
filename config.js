98626e2a2414cb3581bf995c639c1ffed9b9b0e2
zhaolinxu <zhaolinxu@sina.cn>
2019-05-02 09:38:41 +0800

    更新

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 dojo.declare("classes.KGConfig", null, {
     statics: {
         disableWebWorkers: false,
-        locales: ["ru", "ja", "br", "es", "fr", "cz", "zh", "chs"],
+        locales: ["en", "ru", "ja", "br", "es", "fr", "cz", "zh", "chs"],
         isEldermass: false
     }
 });
