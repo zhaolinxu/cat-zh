93f717f7d3e2031b8d187d3f9aea147a5bfe0f4b
bloodrizer <cheshire.panther@gmail.com>
2019-07-26 17:32:33 -0700

    Fix for https://www.reddit.com/r/kittensgame/comments/ci493v/jobs_for_kittens_from_spaaaaaaace/

--- js/buildings.js
+++ js/buildings.js
@@ -458,6 +458,9 @@ dojo.declare("classes.managers.BuildingsManager", com.nuclearunicorn.core.TabMan
 			"maxKittens": 1,
 			"manpowerMax": 50
 		},
+		unlocks: {
+			tabs: ["village"]
+		},
 		breakIronWill: true,
 		flavor : $I("buildings.logHouse.flavor")
 	},{
@@ -473,6 +476,9 @@ dojo.declare("classes.managers.BuildingsManager", com.nuclearunicorn.core.TabMan
 		effects: {
 			"maxKittens": 1,
 			"manpowerMax": 50
+		},
+		unlocks: {
+			tabs: ["village"]
 		},
 		breakIronWill: true,
 		flavor: $I("buildings.mansion.flavor")
