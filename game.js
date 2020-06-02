ef56f78686d04d8ad0bccc24369cd32b6c454e40
Ethaniel Kavelaars <ethaniel.robotik@free.fr>
2020-06-02 19:32:45 +0200

    In compressLZData, make the most common mode the default mode

--- game.js
+++ game.js
@@ -1508,7 +1508,7 @@ dojo.declare("com.nuclearunicorn.game.ui.GamePage", null, {
 		//5mb limit workaround
 		if (saveDataString.length > 5000000 || this.opts.forceLZ) {
 			console.log("compressing the save file...");
-			saveDataString = this.compressLZData(saveDataString);
+			saveDataString = this.compressLZData(saveDataString, true);
 		}
 
 		LCstorage["com.nuclearunicorn.kittengame.savedata"] = saveDataString;
@@ -1576,12 +1576,12 @@ dojo.declare("com.nuclearunicorn.game.ui.GamePage", null, {
 			: LZString.decompressFromUTF16(lzData);
 	},
 
-	compressLZData: function(json, useBase64) {
+	compressLZData: function(json, useUTF16) {
 		//todo check game compatibility flags
 		//console.log("base64 length:", LZString.compressToBase64(json).length, "utf-16 length:", LZString.compressToUTF16(json).length);
-		return useBase64
-			? LZString.compressToBase64(json)
-			: LZString.compressToUTF16(json);
+		return useUTF16
+			? LZString.compressToUTF16(json)
+			: LZString.compressToBase64(json);
 	},
 
 	_parseLSSaveData: function(){
@@ -1707,7 +1707,7 @@ dojo.declare("com.nuclearunicorn.game.ui.GamePage", null, {
 		var data = this.save();
 		data = JSON.stringify(data);
 
-        var encodedData = this.compressLZData(data, true /*base64*/);
+        var encodedData = this.compressLZData(data);
         this.ui.saveExport(encodedData);
 
 	},
@@ -1729,7 +1729,7 @@ dojo.declare("com.nuclearunicorn.game.ui.GamePage", null, {
         var $link = $('#download-link');
 
         var data = JSON.stringify(this.save());
-        var lzdata = this.compressLZData(data, true /*base64*/);
+        var lzdata = this.compressLZData(data);
         var blob = new Blob([lzdata], {type: 'text/plain'});
         $link.attr('href', window.URL.createObjectURL(blob));
 
@@ -1747,7 +1747,7 @@ dojo.declare("com.nuclearunicorn.game.ui.GamePage", null, {
 		this.save();
 		var data = this.save();
 		data = JSON.stringify(data);
-		var lzdata = this.compressLZData(data, true /*base64*/);
+		var lzdata = this.compressLZData(data);
 
 
         var callback = function() {
