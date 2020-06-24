35722b0e179e2d8ca834d1221d38c7aa21efc4fc
PellaAndroid <PellaAndroid@rjmacy.com>
2020-06-24 13:53:48 -0500

    WIP i18n overhaul

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 dojo.declare("classes.KGConfig", null, {
     statics: {
         disableWebWorkers: false,
-        locales: ["ru", "ja", "br", "es", "fr", "cz", "zh", "pl"],
+        locales: ["ru", "ja", "br", "es", "fr", "cz", "zh", "pl", "zh-Hant"],
         schemes: ["dark", "grassy", "sleek", "gold", "space", "wood", "school", "fluid", "vessel", "minimalist", "oil", "unicorn"]
     }
 });
