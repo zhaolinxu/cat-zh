271cf52e81f383d8a3b69f14a118623eb4b5c1d9
AZiggurat <ziggurat0001@protonmail.com>
2020-11-04 19:10:45 +0200

    change void cost to 0.4 for more confusion.

--- js/challenges.js
+++ js/challenges.js
@@ -148,7 +148,7 @@ dojo.declare("classes.managers.ChallengesManager", com.nuclearunicorn.core.TabMa
                         if (self.active) {
                                 self.effects["shatterCostReduction"] = 0;
                                 self.effects["shatterCostIncreaseChallenge"] = 0.5;
-                                self.effects["shatterVoidCost"] = 0.5;
+                                self.effects["shatterVoidCost"] = 0.4;
                         }else{
 							self.effects["shatterCostReduction"] = -0.02;
 							self.effects["shatterCostIncreaseChallenge"] = 0;
@@ -273,7 +273,8 @@ dojo.declare("classes.managers.ChallengesManager", com.nuclearunicorn.core.TabMa
 			$I("challendge.btn.confirmation.msg"), function() 
 		{
 			// Reset with any benefit of chronosphere (resources, kittens, etc...)
-
+			// Should put resources and kittens to reserve HERE!
+			// Kittens won't be put into reserve in post apocalypcis!
 			game.bld.get("chronosphere").val = 0;
 			game.bld.get("chronosphere").on = 0;
 			game.time.getVSU("cryochambers").val = 0;
