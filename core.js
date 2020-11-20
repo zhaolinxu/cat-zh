6de2ae0d37cb4dd8c48c0e27cb22e11dbc1d8d48
AZiggurat <ziggurat0001@protonmail.com>
2020-11-20 12:40:44 +0200

    merce conflict fix 1

--- core.js
+++ core.js
@@ -1938,8 +1938,8 @@ dojo.declare("com.nuclearunicorn.game.ui.BuildingStackableBtnController", com.nu
 
 			if (meta.breakIronWill) {
 				this.game.ironWill = false;
-				var zebraOutpostMeta = game.bld.getBuildingExt("zebraOutpost").meta
-				zebraOutpostMeta.calculateEffects(zebraOutpostMeta, this.game)
+				var liberty = this.game.science.getPolicy("liberty");
+				liberty.calculateEffects(liberty, this.game);
 			}
 
 			if (meta.unlocks) {
