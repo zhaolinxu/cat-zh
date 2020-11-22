c56d13d9d3205ca4b546ba1b739994c9dfdb8e03
AZiggurat <ziggurat0001@protonmail.com>
2020-11-22 16:17:18 +0200

    fix this issue

--- js/challenges.js
+++ js/challenges.js
@@ -402,6 +402,7 @@ dojo.declare("classes.reserveMan", null,{
 		for (var i in this.reserveResources){
 			if(i == "timeCrystal"){
 				delete this.reserveResources[i];
+				continue;
 			}
 			var resCap = this.game.resPool.get(i).maxValue;
 			if(!resCap){
