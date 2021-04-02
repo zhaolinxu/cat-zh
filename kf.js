8df4ab9e1616b64273cbf3cd41a93d9ae082172c
petercheney <809005839@qq.com>
2021-04-02 11:00:15 +0800

    update kf.js.

--- kf.js
+++ kf.js
@@ -5,16 +5,16 @@ $(function(){
     content +='<div id="open_im" class="open-im">&nbsp;</div>  ';
     content +='<div class="im_main" id="im_main">';
     content +='<div id="close_im" class="close-im"><a href="javascript:void(0);" title="点击关闭">&nbsp;</a></div>';
-    content +='<a href="http://shang.qq.com/wpa/qunwpa?idkey=d10d784492d5e73cca25ff48f4763bdc14f698ffdbf42adbb0e9304495cb0387" target="_blank" class="im-qq qq-a" title="点击加入 - GitHub游戏交流群:627141737">';
+    content +='<a href="https://qm.qq.com/cgi-bin/qm/qr?k=tCJDY9A6mYcHFtdu13CrU8OqepUIwR-j&jump_from=webapi" target="_blank" class="im-qq qq-a" title="猫国建设者交流群:499256691">';
     content +='<div class="qq-container"></div>';
     content +='<div class="qq-hover-c"><img class="img-qq" src="//likexia.gitee.io/game/images/kf/qq.png"></div>';
     content +='<span>点击加群</span>';
     content +='</a>';
     content +='<div class="im-tel">';
-    content +='<div>QQ群号:</div>';
-    content +='<div class="tel-num">627141737</div>';
+    content +='<div>猫国QQ群:</div>';
+    content +='<div class="tel-num">499256691</div>';
     content +='<div>更多同类游戏:</div>';
-    content +='<div class="tel-num"><a href="http://www.gityx.com" target="_blank" title="我们有域名啦~">Git游戏</a><a href="http://bbs.gityx.com/" target="_blank" style="margin-left:5px;" title="点击查看游戏攻略">论坛</a><a href="http://likexia.gitee.io/game/" target="_blank" style="margin-left:5px;" title="之前首页继续保留">旧版</a></div>';
+    content +='<div class="tel-num"><a href="http://www.gityx.com" target="_blank" title="Git游戏：gityx.com~">Git游戏</a><a href="http://g8hh.com" target="_blank" style="margin-left:5px;" title="锅巴汉化：g8hh.com">锅巴汉化</a><br /><a href="http://bbs.gityx.com/" target="_blank" style="margin-left:5px;" title="点击查看游戏攻略">游戏攻略交流论坛</a></div>';
     content +='</div>';
     content +='<div class="im-footer" style="position:relative">';
     content +='<div class="weixing-container">';
@@ -23,7 +23,7 @@ $(function(){
     //content +='<img class="weixing-ma" src="//likexia.gitee.io/game/images/kf/zfb-ma.jpg">';
 //    content +='<div class="weixing-txt" style="font-size:13px;">打开手机QQ<br>扫码加群<br>一起讨论吧<br><a href="http://shang.qq.com/wpa/qunwpa?idkey=d10d784492d5e73cca25ff48f4763bdc14f698ffdbf42adbb0e9304495cb0387" target="_blank" title="点击加入 - GitHub游戏交流群:627141737">点击加群</a></div>';
     content +='<div class="weixing-txt" style="font-size:13px;">打开微信扫一扫<br>关注公众号<br><br><a href="http://shang.qq.com/wpa/qunwpa?idkey=d10d784492d5e73cca25ff48f4763bdc14f698ffdbf42adbb0e9304495cb0387" target="_blank" title="点击加入 - Git游戏交流群:627141737">Git游戏</a></div>';
-    content +='<img class="weixing-ma" src="https://likexia.gitee.io/game/images/kf/weixing-ma.jpg">';
+    content +='<img class="weixing-ma" src="http://likexia.gitee.io/game/images/kf/weixing-ma.jpg">';
     content +='<div class="weixing-sanjiao"></div>';
     content +='<div class="weixing-sanjiao-big"></div>';
     content +='</div>';
@@ -35,27 +35,27 @@ $(function(){
     content +='</div>';
     content +='<!-- 客服动态内容 结束 -->';
     $("body").append(content);
-	$('#close_im').bind('click',function(){
-		$('#main-im').css("height","0");
-		$('#im_main').hide();
-		$('#open_im').show();
-	});
-	$('#open_im').bind('click',function(e){
-		$('#main-im').css("height","272");
-		$('#im_main').show();
-		$(this).hide();
-	});
-	$('.go-top').bind('click',function(){
-		$(window).scrollTop(0);
-	});
-	$(".weixing-container").bind('mouseenter',function(){
-		$('.weixing-show').show();
-	})
-	$(".weixing-container").bind('mouseleave',function(){        
-		$('.weixing-show').hide();
-	});
+    $('#close_im').bind('click',function(){
+        $('#main-im').css("height","0");
+        $('#im_main').hide();
+        $('#open_im').show();
+    });
+    $('#open_im').bind('click',function(e){
+        $('#main-im').css("height","272");
+        $('#im_main').show();
+        $(this).hide();
+    });
+    $('.go-top').bind('click',function(){
+        $(window).scrollTop(0);
+    });
+    $(".weixing-container").bind('mouseenter',function(){
+        $('.weixing-show').show();
+    })
+    $(".weixing-container").bind('mouseleave',function(){        
+        $('.weixing-show').hide();
+    });
 
-//	定时关闭
+//  定时关闭
   setTimeout(function () {
     $('#im_main').hide();
   },5000);
