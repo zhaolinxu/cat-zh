5f4aff13593744cb01211b316cc34d00d52a907a
petercheney <809005839@qq.com>
2021-04-09 11:32:05 +0800

    update game.js.

--- game.js
+++ game.js
@@ -195,9 +195,9 @@ dojo.declare("classes.game.Server", null, {
 		});
 
 		//-- fetch UID from KGNet if HTTP session is established ---
-		if (!this.userProfile){
+		/*if (!this.userProfile){
 			this.syncUserProfile();
-		}
+		}*/
 		
 	},
 
