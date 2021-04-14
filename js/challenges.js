25bafa5324e382f4106bab3c31f8d4c9a08499e5
AZiggurat <ziggurat0001@protonmail.com>
2021-04-14 20:54:32 +0300

    fix errors

--- js/challenges.js
+++ js/challenges.js
@@ -232,8 +232,8 @@ dojo.declare("classes.managers.ChallengesManager", com.nuclearunicorn.core.TabMa
 			}
 			var tradepost =game.bld.getBuildingExt("tradepost").meta;
 			var tradeKnowledge = game.getEffect("tradeKnowledge");
-			let tradepostLimit = (7 + tradeKnowledge * 3) * (0.99 + tradeKnowledge * 0.01);
-			let tradepostRatioLimit = game.getLimitedDR(0.099 + tradeKnowledge * 0.0075, 0.25);
+			var tradepostLimit = (7 + tradeKnowledge * 3) * (0.99 + tradeKnowledge * 0.01);
+			var tradepostRatioLimit = game.getLimitedDR(0.099 + tradeKnowledge * 0.0075, 0.25);
 			return (tradepost.effects["tradeRatio"]*Math.min(tradepostLimit, tradepost.val * tradepostRatioLimit));
 		}
 	}],
