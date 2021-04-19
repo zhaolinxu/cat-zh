340d5895a2a015de9931a2f5e5ea214a72f95244
AZiggurat <ziggurat0001@protonmail.com>
2021-04-19 16:27:29 +0300

    make one console command be enough

--- js/buildings.js
+++ js/buildings.js
@@ -1316,7 +1316,7 @@ dojo.declare("classes.managers.BuildingsManager", com.nuclearunicorn.core.TabMan
 
 			effects["energyConsumption"] = 2;
 			//here will be that workshop upgrade, it'll make factories produce twice less pollution
-			if(game.factoryClean){
+			if(game.detailedPollutionInfo){
 				self.isAutomationEnabled = (self.isAutomationEnabled === null)? true: self.isAutomationEnabled;
 			}else{
 				self.isAutomationEnabled = null;
