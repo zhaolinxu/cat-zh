470ad60c51935eeb040fc58d0a76f612930858d4
PeterCheney <809005839@qq.com>
2021-08-03 10:30:06 +0800

    汉化翻译

--- js/achievements.js
+++ js/achievements.js
@@ -214,8 +214,8 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
     badges: [
         {   
             name: "lotus",
-            title: "Lotus Eater",
-            description: "Have more than 50 total resets",
+            title: "醉生梦死",
+            description: "周目数大于50",
             difficulty: "A",
             condition: function(){
                 return this.game.stats.getStat("totalResets").val >= 50;
@@ -223,14 +223,14 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
         },
         {   
             name: "ivoryTower",
-            title: "Ivory Tower",
-            description: "Have a reset in a IW atheism",
+            title: "白色巨塔",
+            description: "钢铁无神论中重置",
             difficulty: "S+"
         },
         {   
             name: "useless",
-            title: "Effective Management",
-            description: "Have a useless leader",
+            title: "管理有方",
+            description: "使用无特质的领袖",
             difficulty: "F",
             condition: function(){
                 var leader = this.game.village.leader;
@@ -239,8 +239,8 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
         },
         { 
             name: "beta",
-            title: "Beta Decay",
-            description: "Participate in a beta test",
+            title: "β测试猿",
+            description: "参加过测试模式",
             difficulty: "B",
             condition: function(){
                 if (window && window.location && window.location.href){
@@ -250,21 +250,21 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
             }
         },{
             name: "silentHill",
-            title: "Silent Hills",
-            description: "Have not MOTD content",
+            title: "静寂岭",
+            description: "获取不到服务器信息",
             difficulty: "S",
             condition: function(){
                 return (this.game.server.motdContent == "");
             }
         },{
             name: "evergreen",
-            title: "Wood badge",
-            description: "Craft a wood I think?",
+            title: "木质奖章",
+            description: "我想你制作了出一块木头？",
             difficulty: "F"
         },{
             name: "deadSpace",
-            title: "Dead Space",
-            description: "Have kittens wander in the void",
+            title: "死亡空间",
+            description: "猫咪在虚空中游荡",
             difficulty: "S",
             condition: function(){
                 var kittens = this.game.resPool.get("kittens");
@@ -272,8 +272,8 @@ dojo.declare("classes.managers.Achievements", com.nuclearunicorn.core.TabManager
             }
         },{
             name: "reginaNoctis",
-            title: "Regina Noctis",
-            description: "Have 500 kittens and no alicorns",
+            title: "夜之女王",
+            description: "拥有500只猫咪且没独角兽",
             difficulty: "S",
             condition: function(){
                 return (this.game.resPool.get("kittens").value > 500 && this.game.resPool.get("alicorn").value == 0);
@@ -438,7 +438,7 @@ dojo.declare("classes.ui.AchievementsPanel", com.nuclearunicorn.game.ui.Panel, {
 				title: ach.starUnlocked ? ach.starDescription : "???"
 			}, span);
 		}
-		divHeader.innerHTML = $I("badges.header", [completedAchievements, totalAchievements]);
+		divHeader.innerHTML = $I("achievements.header", [completedAchievements, totalAchievements]);
 		var stars = "";
 		for (var i = completedStars; i > 0; --i) {
 			stars += "&#9733;";
