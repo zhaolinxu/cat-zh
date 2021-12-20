b7d02b945d3f92f58661cc50d02027d0f726a987
AZiggurat <ziggurat0001@protonmail.com>
2021-12-20 16:13:58 +0200

    make priceRatio of temporalPress decrease based on number of 1000Years completion, growing down to 1.05 at 50+ completions

--- js/time.js
+++ js/time.js
@@ -373,6 +373,7 @@ dojo.declare("classes.managers.TimeManager", com.nuclearunicorn.core.TabManager,
             }
             self.effects["shatterYearBoost"] = (self.isAutomationEnabled)? 5 * game.calendar.yearsPerCycle : game.calendar.yearsPerCycle; //25 or 5 currently
             self.limitBuild = game.getEffect("temporalPressCap");
+            self.priceRatio = Math.max(1.05, 1.1 - game.challenges.getChallenge("1000Years").on * 0.001); //first 50 completions of 1000Years make priceRatio cheaper
         },
         isAutomationEnabled: null,
         unlocked: false
