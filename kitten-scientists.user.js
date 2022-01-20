267237099050dc116f76ac8494db01dd1e30b4ca
Cheney <809005839@qq.com>
2022-01-20 14:33:43 +0800

    更新

--- kitten-scientists.user.js
+++ kitten-scientists.user.js
@@ -1337,7 +1337,7 @@ var run = function() {
                     if (optionVals.timeSkip[(currentCycle + skipCycles) % cyclesPerEra] && canSkip > 0) {willSkip += canSkip;}
                 }
                 if (willSkip > 0) {
-					willSkip = Math.min(willSkip, 2500);
+					willSkip = Math.min(willSkip, Math.max(500 , game.getEffect("temporalPressCap") * 25));
                     optionVals.timeSkip.adore = true;
                     var beforeSkipYear = game.calendar.year;
                     shatter.controller.doShatterAmt(shatter.model, willSkip);
@@ -4472,6 +4472,11 @@ var run = function() {
 
             triggerButton.on('click', function () {
                 var value;
+				if (toggleName === 'faith') {
+					itext += "(" + $I("resources.faith.title")+ ")";
+				} else if (toggleName === 'trade') {
+					itext += "(" + $I("resources.gold.title") + ")";
+				}
                 value = window.prompt(i18n('ui.trigger.set', [itext]), auto.trigger);
 
                 if (value !== null) {
